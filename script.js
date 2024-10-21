016e29643dd09f63386ee5d4d1388e6930ddaec4
Calvin La <calvin.la@clover.com>
2024-10-21 10:21:44 -0700

    Added count and active light functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Write your code here
+let bulb1  = document.querySelector("#lightbulb1");
+let bulb2  = document.querySelector("#lightbulb2");
+let bulb3  = document.querySelector("#lightbulb3");
+let sub = document.querySelector(".subtitle");
+let count = 0;
+bulb1.addEventListener("click", function(){
+    count++;
+    sub.innerHTML = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+});
+bulb2.addEventListener("click", function(){
+    count++;
+    sub.innerHTML = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+
+});
+bulb3.addEventListener("click", function(){
+    count++;
+    sub.innerHTML = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+
+});
